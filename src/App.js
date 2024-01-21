fe795831b9477d98cc20cfe5f6a43d711a53ec00
imsauravsingh <imsauravsingh@gmail.com>
2024-01-22 01:24:19 +0530

    project setup with code lint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>Test</div>
+
+        <div>hello saurav asdfasdf</div>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
